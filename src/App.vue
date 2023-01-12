d4be46c93a1d1801c20e619dc3821ad6dc1ff586
nodejshobby <nodejshobby@gmail.com>
2023-01-12 02:32:04 +0100

    Working on navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavbarSection />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavbarSection from './components/NavbarSection'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+  body{
+    font-family: 'Poppins', sans-serif;
+  }
 </style>
---
+++ src/components/NavbarSection.vue
@@ -0,0 +1,81 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-white">
+  <div class="container-fluid navbar-padding">
+    <a class="navbar-brand" href="/"><img src="../assets/logo.png" height="23" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-4 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Case</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">DaCode Specialists</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
+
+      <a class="cta ms-lg-4" href="#">Request a quote</a>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .nav-link {
+    color: #60E1CB !important;
+  }
+
+ .nav-link:hover {
+    color: #000 !important;
+  }
+
+  .nav-link.active {
+    color: #000 !important;
+  }
+
+ .cta {
+    background: #7BC0E3;
+    border-radius: 1000px;
+    padding: .8rem 1.6rem;
+    color: #fff !important;
+    border : none;
+    text-decoration: none;
+  }
+
+  .cta:hover {
+    color: #fff;
+  }
+
+ 
+
+  /* .nav-link.active {
+    color: #000 !important;
+  } */
+
+  
+
+  @media (min-width: 992px) {
+    .navbar-padding {
+      padding-left: 10rem !important;
+      padding-right: 10rem !important;
+    }
+  }
+ 
+
+</style>
